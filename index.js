ebe92ba04c08dfb7d03723abc33ecddf8db201c2
Mohammad <zia.ro@live.com>
2022-03-27 17:22:11 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+function saturdayFun(fun="roller-skate"){
+    return `This Saturday, I want to ${fun}!`
+}
+// saturdayFun('roller-skate')
+//Bellow function is not completed
+function mondayWork(activity){
+   if (activity === undefined){
+       return "This Monday, I will go to the office."
+
+   }else{
+       return "This Monday, I will work from home."
+   }
+
+}
+
+
+
+ function wrapAdjective(name){
+     if (name === undefined){
+         return "*";
+     }
+     return function (string){
+         if (string === undefined){
+             return "special"
+             
+         }
+         else return `You are ${name}${string}${name}!`
+     }
+ }
